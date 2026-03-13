01fe5cc159dd961d7c57e61b6806072ff15aa074
miro-oss <roxkstar@Roxkstar-2.local>
2026-03-13 15:52:51 +0900

    [SETTING]

---
+++ src/main/java/com/example/elephantfinancelab_be/ElephantfinancelabBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.elephantfinancelab_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElephantfinancelabBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElephantfinancelabBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/elephantfinancelab_be/ElephantfinancelabBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.elephantfinancelab_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElephantfinancelabBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
