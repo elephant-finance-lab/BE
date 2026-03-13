2c248198687774929390d5e8246d9512d9f2f2cd
miro-oss <roxkstar@Roxkstar-2.local>
2026-03-13 16:25:59 +0900

    [SETTING] spotless 세팅

--- src/main/java/com/example/elephantfinancelab_be/ElephantfinancelabBeApplication.java
+++ src/main/java/com/example/elephantfinancelab_be/ElephantfinancelabBeApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ElephantfinancelabBeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ElephantfinancelabBeApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(ElephantfinancelabBeApplication.class, args);
+  }
 }
--- src/test/java/com/example/elephantfinancelab_be/ElephantfinancelabBeApplicationTests.java
+++ src/test/java/com/example/elephantfinancelab_be/ElephantfinancelabBeApplicationTests.java
@@ -6,8 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ElephantfinancelabBeApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+  @Test
+  void contextLoads() {}
 }
