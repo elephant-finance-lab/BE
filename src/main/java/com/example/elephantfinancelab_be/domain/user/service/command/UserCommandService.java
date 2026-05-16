eac1878feda5f4c940a5360cf6aed6253f9b8089
jelee <jelee2204@dgu.ac.kr>
2026-05-16 22:49:45 +0900

    [FEAT] POST /api/users/me API 구현 및 Account 엔티티 nullable 변경
    - UserReqDTO에 RegisterInfo inner class 추가
    - UserResDTO에 UserId inner class 추가
    - User 엔티티에 updateInfo() 메서드 추가
    - Account 엔티티 bankName, accountHolder, accountType nullable 변경
    - UserCommandService에 saveUserInfo() 추가
    - UserController에 POST /api/users/me 엔드포인트 추가

--- src/main/java/com/example/elephantfinancelab_be/domain/user/service/command/UserCommandService.java
+++ src/main/java/com/example/elephantfinancelab_be/domain/user/service/command/UserCommandService.java
@@ -1,10 +1,13 @@
 package com.example.elephantfinancelab_be.domain.user.service.command;
 
 import com.example.elephantfinancelab_be.domain.user.dto.req.UserReqDTO;
+import com.example.elephantfinancelab_be.domain.user.dto.res.UserResDTO;
 
 public interface UserCommandService {
 
   void updateProfile(Long userId, UserReqDTO.UpdateProfile request);
 
   void withdraw(Long userId);
+
+  UserResDTO.UserId saveUserInfo(Long userId, UserReqDTO.RegisterInfo request);
 }
