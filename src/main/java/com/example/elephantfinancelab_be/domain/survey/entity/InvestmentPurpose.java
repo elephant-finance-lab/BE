1f62cf9f60fdfae1e0762236511784455a169e6a
miro-oss <roxkstar@isieun-ui-iMac.local>
2026-05-23 01:44:12 +0900

    [FEAT] 선호도 조사 삭제

--- src/main/java/com/example/elephantfinancelab_be/domain/survey/entity/InvestmentPurpose.java
+++
@@ -1,7 +0,0 @@
-package com.example.elephantfinancelab_be.domain.survey.entity;
-
-public enum InvestmentPurpose {
-  POCKET_MONEY,
-  ASSET_GROWTH,
-  HIGH_RETURN_PURSUIT
-}
--- src/main/java/com/example/elephantfinancelab_be/domain/survey/entity/InvestmentStyle.java
+++
@@ -1,7 +0,0 @@
-package com.example.elephantfinancelab_be.domain.survey.entity;
-
-public enum InvestmentStyle {
-  STABLE_BLUE_CHIP,
-  GROWTH_COMPANY,
-  HIGH_VOLATILITY_STOCK
-}
