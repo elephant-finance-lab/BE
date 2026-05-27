d1d17c18074e2f07c69f0be129d04589222666fd
jelee <jelee2204@dgu.ac.kr>
2026-05-27 13:00:59 +0900

    exp5 load-test URL 변경

--- load-test.js
+++ load-test.js
@@ -4,13 +4,17 @@ import { sleep } from 'k6';
 export const options = {
   stages: [
     { duration: '30s', target: 10 },
-    { duration: '30s', target: 50 },
     { duration: '30s', target: 100 },
+    { duration: '30s', target: 100 },
+    { duration: '30s', target: 300 },
+    { duration: '30s', target: 300 },
+    { duration: '30s', target: 500 },
+    { duration: '30s', target: 500 },
     { duration: '10s', target: 0 },
   ],
 };
 
 export default function () {
-  http.get('http://localhost:8080/api/stocks/005930/summary');
+  http.get('http://localhost:8080/api/stocks/005930/info?period=QUARTER');
   sleep(1);
 }
